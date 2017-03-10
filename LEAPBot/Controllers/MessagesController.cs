f1e5a5dc34c19afc1ead937efd3a6d29754e79f9
Pedro Dias <pedro@digitaldias.com>
2017-03-10 08:12:01 +0100

    Update NuGet Packages

--- LEAPBot/Controllers/MessagesController.cs
+++ LEAPBot/Controllers/MessagesController.cs
@@ -33,10 +33,7 @@ namespace LEAPBot
             _service    = new LuisService(model);
         }
 
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
+
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
@@ -55,15 +52,13 @@ namespace LEAPBot
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
-                // Implement user deletion here
                 // If we handle user deletion, return a real message
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-                
+                // Not available in all channels                
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
